521fb50b2c88bec0dad92852c5e80a2b72f23882
Emir Ponce <patriadecolores@gmail.com>
2020-05-19 19:43:11 -0300

    Dos cuentas bancarias nuevas

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,6 +6,8 @@ public class main {
     /*Crear cuenta para un cliente*/
     public static void main(String[] args){
         Cuenta cuenta001;
+        Cuenta cuenta002;
+        Cuenta cuenta003;
         cuenta001 = new Cuenta("Emir","Aveneu",1000,"Ahorro");
         System.out.println(cuenta001.getNombre());
         System.out.println(cuenta001.getTipo());
